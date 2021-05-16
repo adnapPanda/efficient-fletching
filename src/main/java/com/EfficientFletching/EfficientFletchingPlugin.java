9a5b3cae8c1f004ef1f0d591c0311f8e62cd0fdf
adnapPanda <robin_herlan@yahoo.com>
2021-05-16 10:31:35 +0200

    updated plugin description

--- src/main/java/com/EfficientFletching/EfficientFletchingPlugin.java
+++ src/main/java/com/EfficientFletching/EfficientFletchingPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.EfficientFletching;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -16,13 +16,13 @@ import net.runelite.client.plugins.PluginDescriptor;
 @PluginDescriptor(
 	name = "Example"
 )
-public class ExamplePlugin extends Plugin
+public class EfficientFletchingPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private EfficientFletchingConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	EfficientFletchingConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(EfficientFletchingConfig.class);
 	}
 }
--- src/test/java/com/EfficientFletching/EfficientFletchingPluginTest.java
+++ src/test/java/com/EfficientFletching/EfficientFletchingPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.EfficientFletching;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class EfficientFletchingPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(EfficientFletchingPlugin.class);
 		RuneLite.main(args);
 	}
 }
